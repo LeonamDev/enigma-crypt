149e29f0c7016f8e44ae31165af34ab34a4c9de9
Leonardo de Oliveira Lourenço <bigodation@gmail.com>
2015-05-08 04:23:02 -0300

    Added help message

--- main.c
+++ main.c
@@ -10,6 +10,8 @@ typedef enum
     DECODE
 } file_mode_t;
 
+char* help_message(char *program_name);
+void print_help(char *program_name);
 void user_mode(void);
 void file_mode(char *filename,file_mode_t type);
 
@@ -21,6 +23,25 @@ int main(int argc, char *argv[])
     {
         case 1:
             user_mode();
+            break;
+        case 2:
+            if(strcmp(argv[1],"-penc") == 0)
+            {
+                ;
+            }
+            else if(strcmp(argv[1],"-pdec") == 0)
+            {
+                ;
+            }
+            else if(strcmp(argv[1],"--help") == 0)
+            {
+                print_help(argv[0]);
+                return 0;
+            }
+            else
+            {
+                error = 1;
+            }
             break;
         case 3:
             if(strcmp(argv[1],"-enc") == 0)
@@ -42,15 +63,33 @@ int main(int argc, char *argv[])
 
     if(error)
     {
-        fprintf(stderr, "\n\tERROR\nUses:\n");
-        fprintf(stderr, "\t%s\t\t   * user mode *\n",argv[0]);
-        fprintf(stderr, "\t%s -enc <filename>   * encodes contents of file *\n",argv[0]);
-        fprintf(stderr, "\t%s -dec <filename>   * decodes contents of file *\n",argv[0]);
+        fprintf(stderr, "\n\tERROR");
+        fprintf(stderr, "%s\n", help_message(argv[0]));
         fflush(stderr);
         return 1;
     }
 
     return 0;
+}
+
+char* help_message(char *program_name)
+{
+    static char message[2048];
+
+    sprintf(message, "\nUses:\n");
+    sprintf(message+strlen(message), "\t%s\t\t   * user mode *\n",program_name);
+    sprintf(message+strlen(message), "\t%s -penc             * encodes characters received by piping *\n",program_name);
+    sprintf(message+strlen(message), "\t%s -pdec             * decodes characters received by piping *\n",program_name);
+    sprintf(message+strlen(message), "\t%s -enc <filename>   * encodes contents of file *\n",program_name);
+    sprintf(message+strlen(message), "\t%s -dec <filename>   * decodes contents of file *\n",program_name);
+    sprintf(message+strlen(message), "\t%s --help            * prints this help message *\n",program_name);
+
+    return message;
+}
+
+void print_help(char *program_name)
+{
+    printf("%s",help_message(program_name));
 }
 
 void user_mode(void)
